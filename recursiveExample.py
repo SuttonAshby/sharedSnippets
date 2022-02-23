977ff2e0504496d71837e1b8717e2c801942beba
Ebigirl5 <ebi.martin4@gmail.com>
2022-02-22 21:52:16 -0500

    testing

--- recursiveExample.py
+++ recursiveExample.py
@@ -3,10 +3,11 @@ def recursiveFunction(count):
     if count >= 0:
         print(count)
         count = count - 1
-        #here is where the function calls itself again 
+        #here is where the function calls itself again
         recursiveFunction(count)
-    else: 
+    else:
         print("Boom!")
 
 #invoking the function
 recursiveFunction(10)
+#
